5605592459b7e35a1c751c3b3b089bdcf346fe51
dustinkiselbach <52965183+dustinkiselbach@users.noreply.github.com>
2020-05-13 18:02:55 -0700

    changed a spelling mistake

--- server.js
+++ server.js
@@ -98,6 +98,6 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
